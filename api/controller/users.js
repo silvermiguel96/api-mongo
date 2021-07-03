ab42aa8fa37c5a79dec109b068ff79b5db97392c
silvermiguel96 <silvermiguel96@gmail.com>
2021-07-03 11:03:10 -0500

    cripy

--- api/controller/users.js
+++ api/controller/users.js
@@ -1,4 +1,6 @@
 const UserModel = require("../model/users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   addUser: async function (required, response) {
@@ -29,17 +31,43 @@ module.exports = {
     const password = required.body.password;
 
     //await MyModel.find({ name: 'john', age: { $gte: 18 } }).exec();
-    const UserDatabase = await UserModel.find({email: email}).exec();
+    // const UserDatabase = await UserModel.find({ email: email }).exec();
+    const [UserDatabase] = await UserModel.find({ email: email }).exec();
 
-    if (!UserDatabase.length) {
-      response.status(404).json({message: "no se encontro el usuario"});
+    // if (!UserDatabase.length) {
+    //   response.status(404).json({ message: "no se encontro el usuario" });
+    // }
+
+    if (!UserDatabase) {
+      response.status(404).json({ message: "no se encontro el usuario" });
     }
 
-    if (password === UserDatabase[0].password) {
+    // if (bcrypt.compareSync(password, UserDatabase[0].password)) {
+    //   const token = jwt.sign(
+    //     { id: UserDatabase[0]._id, email: UserDatabase[0].email },
+    if (bcrypt.compareSync(password, UserDatabase.password)) {
+      console.log("UserDatabase", UserDatabase);
+      const token = jwt.sign(
+        { id: UserDatabase._id, email: UserDatabase.email },
+        required.app.get("secretKey"),
+        { expiresIn: "1h" }
+      );
       console.log(UserDatabase);
-      response.json(UserDatabase);
+      response.json({ user: UserDatabase, token: token });
     } else {
-      response.status(404).json({message: "no se ha encontrado usuario y/o contraseña"});
+      response
+        .status(404)
+        .json({ message: "no se ha encontrado usuario y/o contraseña" });
     }
-  }
+  },
+
+  getUsers: async function (required, response) {
+    const userId = required.body.userId;
+    console.log("id del usuario que ingresa", userId);
+    const UsersDatabase = await UserModel.find().exec();
+    if (!UsersDatabase.length) {
+      response.status(404).json({ message: "no se encontro el usuario" });
+    }
+    response.json(UsersDatabase);
+  },
 };
--- server.js
+++ server.js
@@ -3,25 +3,40 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.host || "localhost";
-const mongoose = require('./api/config/database/config')
+const mongoose = require("./api/config/database/config");
 const bodyParser = require("body-parser");
-
-
+const jwt = require("jsonwebtoken");
+app.set("secretKey", process.env.SECRET_KEY || "mongodb");
 
 const dbConnection = mongoose.connection;
-dbConnection.on('error', console.error.bind(console, 'connection error:'));
-dbConnection.once('open', function() {
-    console.log("Conexion a la base de mongo exitosa!")
+dbConnection.on("error", console.error.bind(console, "connection error:"));
+dbConnection.once("open", function () {
+  console.log("Conexion a la base de mongo exitosa!");
 });
 
+function authToken(required, response, next) {
+  let token = "";
+  if (required.headers.authorization) {
+    token = required.headers.authorization.substring(7);
+  }
+  jwt.verify(token, required.app.get("secretKey"), function (error, data) {
+    if (error) {
+      response.status(403).json(error);
+    } else {
+      console.log('data del token', data)
+      required.body.userId = data.id;
+      required.body.userEmaill = data.email;
+      next();
+    }
+  });
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/users', require('./api/routers/user'));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/users", require("./api/routers/user"));
+
+app.use("/users/private", authToken, require("./api/routers/userPrivate"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://${host}:${port}`);
